f55bfcadf239f6d0794f7ca7185b907833a8fca2
Taylor Davidson <taylordavidson@ymail.com>
2018-02-20 17:04:53 -0800

    navbar done

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props=>{
+    return(
+        <div className="center-align">
+            <h1>About Us</h1>
+            <h3>This is about us</h3>
+        </div>
+    )
+}
